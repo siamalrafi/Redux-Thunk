4c7b14d94929bd35dab64eff8e9d33e56d618673
siamalrafi <rahathasandipto@gmail.com>
2023-06-20 18:56:04 +0600

    create store for app

--- moon-tech-starter-pack/src/redux/store.js
+++ moon-tech-starter-pack/src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./reducers/rootReducer";
 
+// create store for app
 const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
