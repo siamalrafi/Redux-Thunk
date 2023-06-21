5ce06b97664574ac6155ae479eba427709ff20a3
siamalrafi <rahathasandipto@gmail.com>
2023-06-21 13:03:47 +0600

    enw

--- moon-tech-starter-pack/src/layout/Main/Main.js
+++ moon-tech-starter-pack/src/layout/Main/Main.js
@@ -3,12 +3,12 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 const Main = () => {
-  return (
-    <div className='px-10'>
-      <Navbar />
-      <Outlet />
-    </div>
-  );
+   return (
+      <div className="px-10">
+         <Navbar />
+         <Outlet />
+      </div>
+   );
 };
 
 export default Main;
