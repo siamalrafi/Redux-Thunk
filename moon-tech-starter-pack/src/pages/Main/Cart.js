9e1ac779622c8960b13a205055c18de5cc3e770f
siamalrafi <rahathasandipto@gmail.com>
2023-06-22 13:12:13 +0600

    new

--- moon-tech-starter-pack/src/pages/Main/Cart.js
+++ moon-tech-starter-pack/src/pages/Main/Cart.js
@@ -3,16 +3,17 @@ import { useSelector } from "react-redux";
 import ProductCard from "../../components/ProductCard";
 
 const Cart = () => {
-  const cart = useSelector((state) => state.cart);
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {cart
-        .sort((a, b) => a._id - b._id)
-        .map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
-    </div>
-  );
+   const cart = useSelector((state) => state.cart);
+   console.log("cart", cart);
+   return (
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+         {cart
+            .sort((a, b) => a._id - b._id)
+            .map((product) => (
+               <ProductCard key={product.model} product={product} />
+            ))}
+      </div>
+   );
 };
 
 export default Cart;
